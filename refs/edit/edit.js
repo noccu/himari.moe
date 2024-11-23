d89259ee366206eabacf28339f11c5170d71d3cc
noccu <37862870+noccu@users.noreply.github.com>
2024-11-23 11:14:30 +0100

    Add editor using specialized local server

---
+++ refs/edit/edit.js
@@ -0,0 +1,128 @@
+import { getAlbums } from "../common.js"
+
+const ALBUMS = await getAlbums()
+const CUR_ALBUM = new URLSearchParams(location.search).get("a")
+/** @type {Set<HTMLElement>} */
+const SELECTION = new Set()
+var ACTIVE = false
+
+const hosts = {
+    "i.imgur.com": ["", /([^\/]+)$/],
+    "pbs.twimg.com": ["tw:", /media\/([^?]+)\?/],
+    "cdn.donmai.us": ["db:", /sample-([^\.]+).jpg/],
+    "cdn.bsky.app": ["bs:", /did:plc:([^@]+)@/]
+}
+
+/** @param {string} href */
+function parseUrl(href) {
+    /** @type {[string, RegExp]} */
+    const data = hosts[(new URL(href)).host]
+    if (!data) return href
+    const [prefix, re] = data
+    return `${prefix}${href.match(re)[1]}`
+}
+
+/** @param {PointerEvent} e */
+function select(e) {
+    if (!ACTIVE) return
+    if (e.target.nodeName == "IMG") {
+        let active = e.target.classList.toggle("editing")
+        if (active) {
+            SELECTION.add(e.target)
+            // e.target.classList.add("editing")
+        }
+        else {
+            SELECTION.delete(e.target)
+            // e.target.classList.remove("editing")
+        }
+        e.stopPropagation()
+    }
+}
+
+/** @param {KeyboardEvent} e */
+function handleKeys(e) {
+    if (!e.shiftKey) {
+        return
+    }
+    switch (e.key.toLowerCase()) {
+        case "e":
+            toggleEditMode()
+            break
+        case "d":
+            remSelection()
+            break
+        case "a":
+            addImages()
+            break
+        case "c":
+            clearSelection()
+    }
+}
+
+function addImages() {
+    switch (SELECTION.size) {
+        case 0:
+            addNewImage(ALBUMS[CUR_ALBUM])
+            clearSelection()
+            break
+        case 1:
+            addToCarousel()
+            break
+        default:
+            alert("Multiple images selected. Clear selection or select a carousel.")
+    }
+}
+
+function addNewImage (collection, msg="Add image to album") {
+    const newImgUrl = prompt(msg)
+    if (!newImgUrl) return
+    const parsedImgUrl = parseUrl(newImgUrl)
+    collection.push(parsedImgUrl)
+    console.log(`Adding ${newImgUrl} to ${CUR_ALBUM} as ${parsedImgUrl}`)
+    console.log(ALBUMS[CUR_ALBUM])
+}
+
+function addToCarousel() {
+    // Only ever 1 ele
+    for (let ele of SELECTION) {
+        if (ele.subIdx == undefined) {
+            alert("Selected image is not a carousel")
+            return
+        }
+        let imgData = ALBUMS[CUR_ALBUM][ele.idx]
+        if (imgData.src) imgData = imgData.src
+        addNewImage(imgData, "Add image to carousel")
+    }
+    clearSelection()
+}
+
+function clearSelection() {
+    SELECTION.clear()
+    for (let card of document.querySelectorAll("img.editing")) {
+        card.classList.remove("editing")
+    }
+}
+
+function remSelection() {
+    for (let ele of SELECTION) {
+        let img = ALBUMS[CUR_ALBUM][ele.idx]
+        if (img.src) img = img.src
+        if (ele.subIdx) img = img[ele.subIdx]
+        console.log(`Removing ${ele.idx} -> ${ele.subIdx} (${img}) from ${CUR_ALBUM} (triggered by ${ele.src})`)
+    }
+    clearSelection()
+}
+
+
+function toggleEditMode() {
+    if (!CUR_ALBUM) {
+        alert("Edit mode can only be used in albums.")
+        return
+    }
+    ACTIVE = !ACTIVE
+    document.body.classList.toggle("editing")
+    if (!ACTIVE) clearSelection()
+}
+
+document.addEventListener("click", select, { capture: true })
+document.addEventListener("keyup", handleKeys, { passive: true })
---
+++ server.mjs
@@ -0,0 +1,79 @@
+import http from "http"
+import fs from "fs"
+import fspath from "path"
+
+const DIR_CACHE = {}
+const MIME_TYPE = {
+    ".html": "text/html",
+    ".js": "text/javascript",
+    ".json": "application/json",
+    ".css": "text/css",
+}
+
+function saveFile(name, data) {
+    // "/refs/albums.json"
+    console.log(`saving ${name}`)
+    writeFileSync(name, data)
+}
+
+/**
+ * @param {http.IncomingMessage} request
+ * @param {http.ServerResponse} response
+ */
+function handleRequest(request, response) {
+    console.log(`${request.method} ${request.url}`)
+    const fullUrl = new URL(`http://localhost:8000${request.url}`)
+    let relPath = fullUrl.pathname.slice(1)
+    if (request.method == "PUT") {
+        let data = ""
+        request.on("data", d => data += d)
+        request.on("end", x => {
+            if (!request.complete) {
+                console.error("Connection terminated unexpectedly")
+                return
+            }
+            console.log(x)
+            saveFile(fullUrl.pathname, data)
+            res.send("saved")
+        })
+    }
+    else if (request.method == "GET") {
+        if (!fs.existsSync(relPath)) {
+            response.writeHead(404)
+            response.end()
+            return
+        }
+        const headers = {}
+        var isDir = DIR_CACHE[relPath],
+            basePath = undefined
+        if (isDir === undefined) {
+            DIR_CACHE[relPath] = isDir = fs.statSync(relPath).isDirectory()
+        }
+        if (isDir) {
+            if (relPath[relPath.length - 1] != "/") {
+                relPath += "/"
+                basePath = relPath
+                // response.writeHead(301, {location: fullUrl.toString()})
+                // response.end()
+                // return
+            }
+            relPath += "index.html"
+        }
+        headers["Content-Type"] = MIME_TYPE[fspath.extname(relPath)] || "text/plain"
+        var data = fs.readFileSync(relPath, { encoding: "utf-8" })
+        if (basePath) {
+            data = data.replace("<head>", `<head><base href="/${basePath}">`)
+        }
+        data = data.replace(
+            "</head>",
+            `<script type="module" src="/refs/edit/edit.js"></script></head>`
+        )
+        response.writeHead(200, headers)
+        response.end(data)
+    }
+}
+
+const server = http.createServer(handleRequest)
+server.listen(8000, () => {
+    console.log("Server listening on port 8000")
+})
