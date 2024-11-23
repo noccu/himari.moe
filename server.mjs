60505ca21531088c6da16210e19735329c4cc8fa
noccu <37862870+noccu@users.noreply.github.com>
2024-11-23 12:22:42 +0100

    Serve all pages correctly

--- server.mjs
+++ server.mjs
@@ -16,12 +16,17 @@ function saveFile(name, data) {
     writeFileSync(name, data)
 }
 
+function notFound(r) {
+    r.writeHead(404)
+    r.end()
+}
+
 /**
  * @param {http.IncomingMessage} request
  * @param {http.ServerResponse} response
  */
 function handleRequest(request, response) {
-    console.log(`${request.method} ${request.url}`)
+    console.log(`${request.method} ${request.url || "/"}`)
     const fullUrl = new URL(`http://localhost:8000${request.url}`)
     let relPath = fullUrl.pathname.slice(1)
     if (request.method == "PUT") {
@@ -38,11 +43,8 @@ function handleRequest(request, response) {
         })
     }
     else if (request.method == "GET") {
-        if (!fs.existsSync(relPath)) {
-            response.writeHead(404)
-            response.end()
-            return
-        }
+        if (relPath == "") relPath = "index.html"
+        if (!fs.existsSync(relPath)) return notFound(response)
         const headers = {}
         var isDir = DIR_CACHE[relPath],
             basePath = undefined
@@ -52,22 +54,26 @@ function handleRequest(request, response) {
         if (isDir) {
             if (relPath[relPath.length - 1] != "/") {
                 relPath += "/"
-                basePath = relPath
+                basePath = "/" + relPath
                 // response.writeHead(301, {location: fullUrl.toString()})
                 // response.end()
                 // return
             }
             relPath += "index.html"
         }
-        headers["Content-Type"] = MIME_TYPE[fspath.extname(relPath)] || "text/plain"
+        if (!fs.existsSync(relPath)) return notFound(response)
+        const ext = fspath.extname(relPath)
+        headers["Content-Type"] = MIME_TYPE[ext] || "text/plain"
         var data = fs.readFileSync(relPath, { encoding: "utf-8" })
-        if (basePath) {
-            data = data.replace("<head>", `<head><base href="/${basePath}">`)
+        if (ext == ".html") {
+            if (basePath) {
+                data = data.replace("<head>", `<head><base href="${basePath}">`)
+            }
+            data = data.replace(
+                "</head>",
+                `<script type="module" src="/refs/edit/edit.js"></script></head>`
+            )
         }
-        data = data.replace(
-            "</head>",
-            `<script type="module" src="/refs/edit/edit.js"></script></head>`
-        )
         response.writeHead(200, headers)
         response.end(data)
     }
